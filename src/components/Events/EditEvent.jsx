a7f193eff68e451656993472c1b727bddd06cdfe
Wasim Hyder <wasim@creativefaze.com>
2023-11-23 20:01:51 +0500

    Prepopulate edit form

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,10 +1,19 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import { fetchEvent } from '../../util/https.js';
+import LoadingIndicator from '../UI/LoadingIndicator.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EditEvent() {
+  const params = useParams();
   const navigate = useNavigate();
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['events', params.id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id })
+  });
 
   function handleSubmit(formData) { }
 
@@ -12,16 +21,41 @@ export default function EditEvent() {
     navigate('../');
   }
 
+  let content;
+
+  if (isPending) {
+    content = <div className='center'>
+      <LoadingIndicator />
+    </div>
+  }
+
+  if (isError) {
+    content = <div className='center'>
+      <ErrorBlock title="Failed to load event" message={error.info?.message || 'Coud not load event data'} />
+      <div className='form-actions'>
+        <Link to="../" className='button'>
+          Okay
+        </Link>
+      </div>
+    </div>
+  }
+
+  if (data) {
+    content = <EventForm inputData={data} onSubmit={handleSubmit}>
+      <Link to="../" className="button-text">
+        Cancel
+      </Link>
+      <button type="submit" className="button">
+        Update
+      </button>
+    </EventForm>
+  }
+
+
+
   return (
     <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
-      </EventForm>
+      {content}
     </Modal>
   );
 }
