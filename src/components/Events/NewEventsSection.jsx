7175bbc2c08bd4cfd81effe91e5505bedef7b7b8
Wasim Hyder <wasim@creativefaze.com>
2023-11-23 20:28:47 +0500

    Limited number of events

--- src/components/Events/NewEventsSection.jsx
+++ src/components/Events/NewEventsSection.jsx
@@ -7,8 +7,8 @@ import { fetchEvents } from '../../util/https.js';
 
 export default function NewEventsSection() {
   const { data, isPending, isError, error } = useQuery({
-    queryKey: ['events'],
-    queryFn: fetchEvents,
+    queryKey: ['events', { max: 3 }],
+    queryFn: ({ signal, queryKey }) => fetchEvents({ signal, ...queryKey[1] }),
     staleTime: 5000, // Time after which a request will be sent.
     //gcTime: 30000, => Time to store cached data
   });
